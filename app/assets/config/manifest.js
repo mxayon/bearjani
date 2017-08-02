5229fefa3af83bbd0271aee00717cfc2e1ad4a22
sophn11 <sophianakpil@gmail.com>
2017-08-01 17:23:08 -0700

    loads parallax homepage

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,13 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+
+
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
